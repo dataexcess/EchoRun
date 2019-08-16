a9b8f9c3379095470389078972b5753e170d24f9
dataexcess <admin@dataexcess.xyz>
2019-08-16 12:52:34 +0200

    Project setup

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '9.0'
+
+target 'innate' do
+
+  use_frameworks!
+
+  # Pods for innate
+  pod 'Alamofire'
+  pod 'SwiftyJSON'
+
+end
---
+++ innate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  innate
+//
+//  Created by Joeri Bultheel on 16/08/2019.
+//  Copyright © 2019 dataexcess. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

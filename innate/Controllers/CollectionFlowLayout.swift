a3c883b74f5c07b38e027747a80b3117f01b0a18
dataexcess <admin@dataexcess.xyz>
2020-01-05 18:23:45 +0100

    Collectionview instead of Stackview start implementation

---
+++ innate/Controllers/CollectionFlowLayout.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionFlowLayout.swift
+//  innate
+//
+//  Created by dataexcess on 05.01.20.
+//  Copyright © 2020 dataexcess. All rights reserved.
+//
+
+import UIKit
+
+class CollectionFlowLayout: UICollectionViewFlowLayout {
+
+}
